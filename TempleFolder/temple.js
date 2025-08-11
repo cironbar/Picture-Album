a66d9b5307da4f3cc38f4a74863579f6fe792cce
Your Name <your.email@example.com>
2025-08-11 19:35:40 +0100

    temple

---
+++ TempleFolder/temple.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    let nav = document.getElementById("navMenu");
+    let btn = document.getElementById(".hamburger");
+    
+    nav.classList.toggle("show"); 
+    btn.classList.toggle("active");
+
+
+} 
